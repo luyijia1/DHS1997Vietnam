5b69066b57cbcf6f21eadce8aba9a7a05fbe2c3b
luyijia1 <99098865+luyijia1@users.noreply.github.com>
2022-04-02 08:39:28 -0400

    Update 00-simulation.R

--- scripts/00-simulation.R
+++ scripts/00-simulation.R
@@ -1,1 +1,17 @@
+# Contact: yijia.lu@mail.utoronto.ca
+# Purpose: Download the Demographic and Health Survery (DHS) of Vietnam in 1997
+# Use R Projects, not setwd()
+
+#### Workspace setup ####
+library(janitor)
+library(pdftools)
+library(purrr)
+library(tidyverse)
+library(stringi)
+
+download.file(
+  "https://dhsprogram.com/pubs/pdf/FR104/FR104.pdf", 
+  "1997_Vietnam_DHS.pdf",
+  mode="wb")
+
 
